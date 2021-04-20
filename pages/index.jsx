ba2e64651400c4dc98291a7f9f2f0b22a871ae5f
jtdemey <johntdemey@live.com>
2021-04-19 21:29:20 -0400

    It lives

---
+++ pages/index.jsx
@@ -0,0 +1,10 @@
+import PageHead from "../components/PageHead";
+
+const HomePage = () => (
+  <>
+    <PageHead title="JT | Home" styleLinks={['homepage/home.css']} />
+      <div>oi</div>
+  </>
+);
+
+export default HomePage;
